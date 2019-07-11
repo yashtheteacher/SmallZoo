0aad08a50269dde014fe969debc81d3fc50104bf
ysheth <ysheth@E207T.CEGEPGIM.LCL>
2019-07-11 14:32:57 -0400

    Initial Commit

---
+++ src/smallzoo/SmallZoo.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package smallzoo;
+
+/**
+ *
+ * @author ysheth
+ */
+public class SmallZoo {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("THIS IS THE SMALL ZOO.");
+    }
+    
+}
