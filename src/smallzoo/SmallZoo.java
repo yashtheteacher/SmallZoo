dba69271927d98a477e5db7ccae4872f1af8daf1
ysheth <ysheth@E207T.CEGEPGIM.LCL>
2019-07-11 14:34:01 -0400

    Made some changes

--- src/smallzoo/SmallZoo.java
+++ src/smallzoo/SmallZoo.java
@@ -16,6 +16,7 @@ public class SmallZoo {
      */
     public static void main(String[] args) {
         System.out.println("THIS IS THE SMALL ZOO.");
+        System.out.println("WELCOME....");
     }
     
 }
